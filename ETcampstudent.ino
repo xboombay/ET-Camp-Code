4fe04d9232d80c8bebaaee8298f742b4caa21d6f
Hathairat Chiawchan <38888378+xboombay@users.noreply.github.com>
2025-02-01 12:21:35 +0700

    Update ETcampstudent.ino

--- ETcampstudent.ino
+++ ETcampstudent.ino
@@ -1,33 +1,45 @@
 #include <Adafruit_GFX.h>
-#include <ESP_Adafruit_SSD1306.h>
-#define OLED_RESET 4
+#include <Adafruit_SSD1306.h>
 #include "DHT.h"
-#define DHTPIN D5     
-#define DHTTYPE DHT22 
 
-Adafruit_SSD1306 display(OLED_RESET);
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define OLED_ADDR 0x3C
+
+#define DHTPIN 14
+#define DHTTYPE DHT22
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  display.begin(SSD1306_SWITCHCAPVCC, 0x78>>1);
-  display.display();
-  delay(2000);
-  display.clearDisplay();  
+  Serial.begin(115200);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    Serial.println("ไม่พบ OLED!");
+    for (;;);
+  }
+  Serial.println("OLED พร้อมใช้งาน");
+  display.clearDisplay();
+
   dht.begin();
+  Serial.println("DHT22 พร้อมใช้งาน");
 }
 
 void loop() {
   delay(2000);
+
   float f = dht.readTemperature(true);
-  float t = dht.readTemperature();
+
+  Serial.print("อุณหภูมิ: ");
+  Serial.print(f);
+  Serial.println("°F)");
+
+  display.clearDisplay();
   display.setTextSize(1);
-  display.setTextColor(WHITE);  
-  display.println("F"); 
-  display.println(f);
+  display.setTextColor(WHITE);
   display.setCursor(0, 0);
+  display.println("Temp (F): ");
+  display.println(f);
   display.display();
-  delay(1000);
-  display.clearDisplay(); 
-
-
 }
